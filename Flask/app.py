8328da26ea124af3c28a9a36a142074616403711
Thomas Payton <trpayton@stu.naperville203.org>
2020-10-05 00:03:59 -0500

    Added lootboxes on flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, random
 from pymongo import MongoClient
 from bson.json_util import dumps
 from flask import Flask, render_template, url_for, request, redirect, flash, request, send_from_directory, jsonify
@@ -7,6 +7,42 @@ mongo = MongoClient("mongodb+srv://thomas:{DB_PASSWORD}@cluster0.640kn.mongodb.n
 db = mongo.jokemon
 
 app = Flask(__name__)
+
+rarity_vals = [
+    {
+        "title":"Joesus Christ",
+        "color":0
+    },
+    {
+        "title":"Mythic",
+        "color": 15401215
+    },
+    {
+        "title":"Legendary",
+        "color": 5375
+    },
+    {
+        "title":"Rare",
+        "color": 16711680
+    },
+    {
+        "title":"Special",
+        "color": 65321
+    },
+    {
+        "title":"Fundamental",
+        "color": 5127936
+    },
+]
+
+rarity_weights = [
+    0.0001,
+    0.0499,
+    0.1, 
+    0.15,
+    0.3,
+    0.4 
+]
 
 # Main Routes
 
@@ -21,7 +57,6 @@ def index():
 @app.route('/cards')
 def cards():
     joes = json.loads(dumps(list(db.jokemon.find())))
-    joes = list(sorted(joes, key=lambda joe: joe['rarity']['title'], reverse=True))[::-1]
     joesus = []
     mythic = []
     legendary = []
@@ -55,6 +90,31 @@ def leaderboard():
 @app.route('/rarity')
 def rarity():
     return render_template("rarity.html")
+
+@app.route('/lootbox')
+def lootbox():
+    joes = json.loads(dumps(list(db.jokemon.find())))
+    joesus = []
+    mythic = []
+    legendary = []
+    rare = []
+    special = []
+    fundamental = []
+    for joe in joes:
+        if joe['rarity']['title'] == 'Mythic':
+            mythic.append(joe)
+        elif joe['rarity']['title'] == 'Legendary':
+            legendary.append(joe)
+        elif joe['rarity']['title'] == 'Rare':
+            rare.append(joe)
+        elif joe['rarity']['title'] == 'Special':
+            special.append(joe)
+        elif joe['rarity']['title'] == 'Fundamental':
+            fundamental.append(joe)
+        elif joe['rarity']['title'] == 'Joesus Christ':
+            joe['image_url'] = 'https://www.digiseller.ru/preview/599286/p1_2134247_b8bd3e19.png'
+            joesus.append(joe)
+    return render_template("lootbox.html", card_rarities=[joesus,mythic,legendary,rare,special,fundamental])
 
 # API
 @app.route('/api/leaderboard')
